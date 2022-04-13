140fbfd9141bdecce9b6c3091a92d8cadf30d761
yadab-ecl <yadab-ecl@experts.com.bd>
2022-04-13 22:13:43 +0600

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -195,153 +195,153 @@ const Home: NextPage<PageProps> = ({ section }) => {
     homeAnimation(completeAnimation);
   }, []);
 
-  useEffect(() => {
-    let canvas: any = document.getElementsByTagName("canvas")[0];
-    if (canvas) {
-      canvas.width = document.body.clientWidth;
-      canvas.height = document.body.clientHeight;
+  // useEffect(() => {
+  //   let canvas: any = document.getElementsByTagName("canvas")[0];
+  //   if (canvas) {
+  //     canvas.width = document.body.clientWidth;
+  //     canvas.height = document.body.clientHeight;
 
-      var ctx = canvas.getContext("2d");
-      var characterList = [
-        "a",
-        "b",
-        "c",
-        "d",
-        "e",
-        "f",
-        "g",
-        "h",
-        "i",
-        "j",
-        "k",
-        "l",
-        "m",
-        "n",
-        "o",
-        "p",
-        "q",
-        "r",
-        "s",
-        "t",
-        "u",
-        "v",
-        "w",
-        "x",
-        "y",
-        "z",
-      ];
+  //     var ctx = canvas.getContext("2d");
+  //     var characterList = [
+  //       "a",
+  //       "b",
+  //       "c",
+  //       "d",
+  //       "e",
+  //       "f",
+  //       "g",
+  //       "h",
+  //       "i",
+  //       "j",
+  //       "k",
+  //       "l",
+  //       "m",
+  //       "n",
+  //       "o",
+  //       "p",
+  //       "q",
+  //       "r",
+  //       "s",
+  //       "t",
+  //       "u",
+  //       "v",
+  //       "w",
+  //       "x",
+  //       "y",
+  //       "z",
+  //     ];
 
-      //stocks possible character attributes
-      var layers = {
-        n: 5, //number of layers
-        letters: [100, 40, 30, 20, 10], //letters per layer (starting from the deepest layer)
-        coef: [0.1, 0.2, 0.4, 0.6, 0.8], //how much the letters move from the mouse (starting from the deepest layer)
-        size: [16, 22, 36, 40, 46], //font size of the letters (starting from the deepest layer)
-        color: ["#fff", "#eee", "#ccc", "#bbb", "#aaa"], //color of the letters (starting from the deepest layer)
-        font: "Courier", //font family (of every layer)
-      };
+  //     //stocks possible character attributes
+  //     var layers = {
+  //       n: 5, //number of layers
+  //       letters: [100, 40, 30, 20, 10], //letters per layer (starting from the deepest layer)
+  //       coef: [0.1, 0.2, 0.4, 0.6, 0.8], //how much the letters move from the mouse (starting from the deepest layer)
+  //       size: [16, 22, 36, 40, 46], //font size of the letters (starting from the deepest layer)
+  //       color: ["#fff", "#eee", "#ccc", "#bbb", "#aaa"], //color of the letters (starting from the deepest layer)
+  //       font: "Courier", //font family (of every layer)
+  //     };
 
-      /*End of options*/
+  //     /*End of options*/
 
-      var characters: any = [];
-      var mouseX = document.body.clientWidth / 2;
-      var mouseY = document.body.clientHeight / 2;
+  //     var characters: any = [];
+  //     var mouseX = document.body.clientWidth / 2;
+  //     var mouseY = document.body.clientHeight / 2;
 
-      var rnd = {
-        btwn: function (min: number, max: number) {
-          return Math.floor(Math.random() * (max - min) + min);
-        },
-        choose: function (list: string | any[]) {
-          return list[rnd.btwn(0, list.length)];
-        },
-      };
+  //     var rnd = {
+  //       btwn: function (min: number, max: number) {
+  //         return Math.floor(Math.random() * (max - min) + min);
+  //       },
+  //       choose: function (list: string | any[]) {
+  //         return list[rnd.btwn(0, list.length)];
+  //       },
+  //     };
 
-      /*LETTER DRAWING*/
+  //     /*LETTER DRAWING*/
 
-      function drawLetter(char: {
-        size: string;
-        font: string;
-        color: any;
-        posX: number;
-        coef: number;
-        posY: number;
-        char: any;
-      }) {
-        ctx.font = char.size + "px " + char.font;
-        ctx.fillStyle = char.color;
+  //     function drawLetter(char: {
+  //       size: string;
+  //       font: string;
+  //       color: any;
+  //       posX: number;
+  //       coef: number;
+  //       posY: number;
+  //       char: any;
+  //     }) {
+  //       ctx.font = char.size + "px " + char.font;
+  //       ctx.fillStyle = char.color;
 
-        var x = char.posX + (mouseX - canvas.width / 2) * char.coef;
-        var y = char.posY + (mouseY - canvas.height / 2) * char.coef;
+  //       var x = char.posX + (mouseX - canvas.width / 2) * char.coef;
+  //       var y = char.posY + (mouseY - canvas.height / 2) * char.coef;
 
-        ctx.fillText(char.char, x, y);
-      }
+  //       ctx.fillText(char.char, x, y);
+  //     }
 
-      /*ANIMATION*/
+  //     /*ANIMATION*/
 
-      document.onmousemove = function (ev) {
-        mouseX = ev.pageX - canvas.offsetLeft;
-        mouseY = ev.pageY - canvas.offsetTop;
+  //     document.onmousemove = function (ev) {
+  //       mouseX = ev.pageX - canvas.offsetLeft;
+  //       mouseY = ev.pageY - canvas.offsetTop;
 
-        if (window.requestAnimationFrame as any) {
-          requestAnimationFrame(update);
-        } else {
-          update();
-        }
-      };
+  //       if (window.requestAnimationFrame as any) {
+  //         requestAnimationFrame(update);
+  //       } else {
+  //         update();
+  //       }
+  //     };
 
-      function update() {
-        clear();
-        render();
-      }
+  //     function update() {
+  //       clear();
+  //       render();
+  //     }
 
-      function clear() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-      }
+  //     function clear() {
+  //       ctx.clearRect(0, 0, canvas.width, canvas.height);
+  //     }
 
-      function render() {
-        for (var i = 0; i < characters.length; i++) {
-          drawLetter(characters[i]);
-        }
-      }
+  //     function render() {
+  //       for (var i = 0; i < characters.length; i++) {
+  //         drawLetter(characters[i]);
+  //       }
+  //     }
 
-      /*INITIALIZE*/
+  //     /*INITIALIZE*/
 
-      function createLetters() {
-        for (var i = 0; i < layers.n; i++) {
-          for (var j = 0; j < layers.letters[i]; j++) {
-            var character = rnd.choose(characterList);
-            var x = rnd.btwn(0, canvas.width);
-            var y = rnd.btwn(0, canvas.height);
+  //     function createLetters() {
+  //       for (var i = 0; i < layers.n; i++) {
+  //         for (var j = 0; j < layers.letters[i]; j++) {
+  //           var character = rnd.choose(characterList);
+  //           var x = rnd.btwn(0, canvas.width);
+  //           var y = rnd.btwn(0, canvas.height);
 
-            characters.push({
-              char: character,
-              font: layers.font,
-              size: layers.size[i],
-              color: layers.color[i],
-              layer: i,
-              coef: layers.coef[i],
-              posX: x,
-              posY: y,
-            });
-          }
-        }
-      }
+  //           characters.push({
+  //             char: character,
+  //             font: layers.font,
+  //             size: layers.size[i],
+  //             color: layers.color[i],
+  //             layer: i,
+  //             coef: layers.coef[i],
+  //             posX: x,
+  //             posY: y,
+  //           });
+  //         }
+  //       }
+  //     }
 
-      createLetters();
-      update();
+  //     createLetters();
+  //     update();
 
-      /*REAJUST CANVAS AFTER RESIZE*/
+  //     /*REAJUST CANVAS AFTER RESIZE*/
 
-      window.onresize = function () {
-        location.reload();
-      };
+  //     window.onresize = function () {
+  //       location.reload();
+  //     };
 
-      (document.getElementById("close") as any).onclick = function () {
-        this.parentElement.style.visibility = "hidden";
-        this.parentElement.style.opacity = "0";
-      };
-    }
-  }, []);
+  //     (document.getElementById("close") as any).onclick = function () {
+  //       this.parentElement.style.visibility = "hidden";
+  //       this.parentElement.style.opacity = "0";
+  //     };
+  //   }
+  // }, []);
 
   return (
     <motion.div
