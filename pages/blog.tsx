19e125a831126d35be885d717971266fdb40e2c9
yadab-ecl <yadab-ecl@experts.com.bd>
2022-05-11 00:46:30 +0600

    new dev to blog added

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -72,7 +72,7 @@ const BlogPage = () => {
       <>
         <PageTitle title="Blog" description={""} items={sites} />
         <motion.div
-          className="mt-10 lg:mt-20 flex flex-col items-start pb-12"
+          className="mt-10 flex flex-col items-start pb-12 lg:mt-20"
           variants={variants}
           initial="init"
           animate="enter"
@@ -82,14 +82,23 @@ const BlogPage = () => {
             href="https://medium.com/@yadabsd/javascript-map-vs-foreach-vs-filter-vs-find-32eff92ca260"
             target="_blank"
             rel="noreferrer noopender"
-            className="lg:w-3/4 mb-12"
+            className="mb-12 lg:w-3/4"
             onHoverStart={() => setHovered(true)}
             onHoverEnd={() => setHovered(false)}
             variants={child}
+            initial={{
+              x: -100,
+            }}
+            whileInView={{
+              x: 0,
+              transition: {
+                duration: .5
+              }
+            }}
           >
-            <div className="mr-auto flex flex-col-reverse lg:flex-row justify-between p-8 lg:p-12 shadow-lg hover:scale-[1.01] transition-transform duration-300">
+            <div className="mr-auto flex flex-col-reverse justify-between p-8 shadow-lg transition-transform duration-300 hover:scale-[1.01] lg:flex-row lg:p-12">
               <div className="lg:mr-12">
-                <h4 className="mb-4 text-xl lg:text-2xl font-semibold">
+                <h4 className="mb-4 text-xl font-semibold lg:text-2xl">
                   Javascript map vs forEach vs filter vs find
                 </h4>
                 <p className="line-clamp-3 three-line mb-4 text-sm text-accent-4">
@@ -129,7 +138,78 @@ const BlogPage = () => {
                 <img
                   src="https://miro.medium.com/fit/c/224/224/1*8Y_ZDPTAN9nySsagDA5ggQ.png"
                   alt=""
-                  className="min-w-content block w-1/2 w-20 lg:w-96 mb-4 lg:mb-0"
+                  className="min-w-content mb-4 block h-full w-1/2 w-20 object-contain  lg:mb-0 lg:w-96"
+                />
+              </div>
+            </div>
+          </motion.a>
+
+          <motion.a
+            href="https://dev.to/yadabsd/javascript-array-and-string-common-methods-1oo0"
+            target="_blank"
+            rel="noreferrer noopender"
+            className="mb-12 ml-auto lg:w-3/4"
+            onHoverStart={() => setHovered(true)}
+            onHoverEnd={() => setHovered(false)}
+            initial={{
+              x: 100,
+            }}
+            whileInView={{
+              x: 0,
+              transition: {
+                duration: .5
+              }
+            }}
+          >
+            <div className="mr-auto flex flex-col-reverse justify-between p-8 shadow-lg transition-transform duration-300 hover:scale-[1.01] lg:flex-row lg:p-12">
+              <div className="lg:mr-12">
+                <h4 className="mb-4 text-xl font-semibold lg:text-2xl">
+                  JavaScript Array and String common methods.
+                </h4>
+                <p className="line-clamp-3 three-line mb-4 text-sm text-accent-4">
+                  JavaScript is an awesome language with a lot of built-in
+                  objects (Array, Date, Math, and String) and their build-in
+                  methods (split, sort, parseInt) which make a developer's life
+                  easier.
+                </p>
+                <div className="flex flex-wrap items-center">
+                  <span className="mr-4 mb-2 text-sm font-bold">Topic: </span>
+                  <div className="flex">
+                    <span
+                      className="mb-2 mr-2 block rounded px-2 py-1 text-sm text-xs font-bold"
+                      style={{
+                        color: colors[0],
+                        border: `1px solid ${colors[0]}`,
+                      }}
+                    >
+                      Javascript
+                    </span>
+                    <span
+                      className="mb-2 mr-2 block rounded px-2 py-1 text-sm text-xs font-bold"
+                      style={{
+                        color: colors[3],
+                        border: `1px solid ${colors[3]}`,
+                      }}
+                    >
+                      Array
+                    </span>
+                    <span
+                      className="mb-2 mr-2 block rounded px-2 py-1 text-sm text-xs font-bold"
+                      style={{
+                        color: colors[4],
+                        border: `1px solid ${colors[4]}`,
+                      }}
+                    >
+                      String
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <img
+                  src="https://res.cloudinary.com/practicaldev/image/fetch/s--dLTfDWuS--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/v22saqzfbln13erwz4q2.png"
+                  alt=""
+                  className="min-w-content mb-4 block h-full w-1/2 w-20 object-contain lg:mb-0 lg:w-96"
                 />
               </div>
             </div>
